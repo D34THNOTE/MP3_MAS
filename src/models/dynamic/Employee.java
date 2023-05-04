3bcedcbc4e310d2218d535edf57ef8159e901125
Bartosz Janowski <detnotatka@gmail.com>
2023-05-04 19:26:08 +0200

    Dynamic inheritance done (3/5)

--- src/models/dynamic/Employee.java
+++ src/models/dynamic/Employee.java
@@ -1,6 +1,8 @@
 package models.dynamic;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.EnumSet;
 
 public class Employee {
 
@@ -25,7 +27,7 @@ public class Employee {
                     String firstName, String lastName, LocalDate birthDate,  // Common
                     String managedDepartment, // Manager
                     String department, String degree, // Engineer
-                    int commission) // Salesperson
+                    Integer commission) // Salesperson
     {
         setEmployeeType(employeeType);
 
@@ -49,10 +51,53 @@ public class Employee {
         return employeeType;
     }
 
-    public void setEmployeeType(EmployeeType employeeType) {
+    private void setEmployeeType(EmployeeType employeeType) { // private setter for initialization
         if(employeeType == null) throw new IllegalArgumentException("Type of the employee is required");
+        if(!EnumSet.of(EmployeeType.MANAGER, EmployeeType.ENGINEER, EmployeeType.SALESPERSON).contains(employeeType))
+            throw new IllegalArgumentException("Invalid employee type");
 
         this.employeeType = employeeType;
+    }
+
+    public void changeEmployeeType(EmployeeType employeeType,
+                                   String managedDepartment, // Manager
+                                   String department, String degree, // Engineer
+                                   Integer commission) // Salesperson
+    {
+        if(employeeType == null) throw new IllegalArgumentException("Type of the employee is required");
+        if(!EnumSet.of(EmployeeType.MANAGER, EmployeeType.ENGINEER, EmployeeType.SALESPERSON).contains(employeeType))
+            throw new IllegalArgumentException("Invalid employee type");
+
+        // validating that incoming data is good before performing operations on the current attributes
+        validateNewData(employeeType,
+                        managedDepartment,
+                        department, degree,
+                        commission);
+
+        // removing attributes
+        switch (this.employeeType) {
+            case MANAGER -> this.managedDepartment = null;
+
+            case ENGINEER -> {
+                this.department = null;
+                this.degree = null;
+            }
+
+            case SALESPERSON -> this.commission = null;
+        }
+
+        // assigning new employee type
+        setEmployeeType(employeeType);
+
+        // assigning new attributes
+        switch (employeeType) {
+            case MANAGER -> setManagedDepartment(managedDepartment);
+            case ENGINEER -> {
+                setDepartment(department);
+                setDegree(degree);
+            }
+            case SALESPERSON -> setCommission(commission);
+        }
     }
 
 
@@ -64,6 +109,8 @@ public class Employee {
     }
 
     public void setFirstName(String firstName) {
+        if(firstName == null || firstName.isBlank()) throw new IllegalArgumentException("Employee's first name is required");
+
         this.firstName = firstName;
     }
 
@@ -72,6 +119,8 @@ public class Employee {
     }
 
     public void setLastName(String lastName) {
+        if(lastName == null || lastName.isBlank()) throw new IllegalArgumentException("Employee's last name is required");
+
         this.lastName = lastName;
     }
 
@@ -80,6 +129,9 @@ public class Employee {
     }
 
     public void setBirthDate(LocalDate birthDate) {
+        if(birthDate == null) throw new IllegalArgumentException("Employee's birth date is required");
+        if(birthDate.isBefore(LocalDate.now())) throw new IllegalArgumentException("Employee's birth date cannot be the present day or a date past it");
+
         this.birthDate = birthDate;
     }
 
@@ -92,6 +144,8 @@ public class Employee {
     }
 
     public void setManagedDepartment(String managedDepartment) {
+        if(managedDepartment == null || managedDepartment.isBlank()) throw new IllegalArgumentException("The department the manager manages is required");
+
         this.managedDepartment = managedDepartment;
     }
 
@@ -104,6 +158,8 @@ public class Employee {
     }
 
     public void setDepartment(String department) {
+        if(department == null || department.isBlank()) throw new IllegalArgumentException("Engineer's department is required");
+
         this.department = department;
     }
 
@@ -112,6 +168,8 @@ public class Employee {
     }
 
     public void setDegree(String degree) {
+        if(degree == null || degree.isBlank()) throw new IllegalArgumentException("Engineer's degree is required");
+
         this.degree = degree;
     }
 
@@ -119,11 +177,34 @@ public class Employee {
 
     // SALESPERSON
 
-    public int getCommission() {
+    public Integer getCommission() {
         return commission;
     }
 
-    public void setCommission(int commission) {
+    public void setCommission(Integer commission) {
+        if(commission == null) throw new IllegalArgumentException("Salesman's commission is required");
+
         this.commission = commission;
+    }
+
+
+    private void validateNewData(EmployeeType employeeType,
+                                       String managedDepartment, // Manager
+                                       String department, String degree, // Engineer
+                                       Integer commission) // Salesperson
+    {
+        switch (employeeType) {
+            case MANAGER -> {
+                if(managedDepartment == null || managedDepartment.isBlank())
+                    throw new IllegalArgumentException("Managed department is mandatory when changing to Manager");
+            }
+            case ENGINEER -> {
+                if(department == null || department.isBlank()) throw new IllegalArgumentException("Department is mandatory when changing to Engineer");
+                if(degree == null || degree.isBlank()) throw new IllegalArgumentException("Degree is mandatory when changing to Engineer");
+            }
+            case SALESPERSON -> {
+                if(commission == null || commission > 0) throw new IllegalArgumentException("Commission percentage cannot be a negative number!");
+            }
+        }
     }
 }
